8a1bf09134c20973f03508972bec29ae7574f879
Clément Baconnier <clement@melba.ch>
2017-11-27 14:43:42 +0100

    Modify Discount resource to be consistent with other resources

--- src/Bexio/Resource/Discount.php
+++ src/Bexio/Resource/Discount.php
@@ -6,55 +6,116 @@ use Bexio\Bexio;
 
 class Discount extends Bexio {
 
+    protected $parentId = null;
+    protected $parentResource = null;
+
+    /**
+     * Set the desired order for the discount scope
+     *
+     * @param \stdClass|integer $order
+     * @return $this
+     */
+    public function setOrder($order)
+    {
+        $this->setParentId($order);
+        $this->setParentResource('kb_order');
+        return $this;
+    }
+
+    /**
+     * Set the desired invoice for the discount scope
+     *
+     * @param \stdClass|integer $order
+     * @return $this
+     */
+    public function setInvoice($order)
+    {
+        $this->setParentId($order);
+        $this->setParentResource('kb_invoice');
+        return $this;
+    }
+
+    /**
+     * Set the desired estimate for the discount scope
+     *
+     * @param \stdClass|integer $order
+     * @return $this
+     */
+    public function setEstimate($order)
+    {
+        $this->setParentId($order);
+        $this->setParentResource('kb_estimate');
+        return $this;
+    }
+
+    /**
+     * @param \stdClass|integer $parent
+     * @return $this
+     */
+    public function setParentId($parent)
+    {
+        if ($parent instanceof \stdClass) {
+            $this->parentId = $parent->id;
+            return $this;
+        }
+        $this->parentId = $parent;
+        return $this;
+    }
+
+    /**
+     * Set de default resource. Can be kb_order|kb_invoice|kb_estimate
+     *
+     * @param string $resource
+     * @return $this
+     */
+    public function setParentResource($resource)
+    {
+        $this->parentResource = $resource;
+        return $this;
+    }
+
     /**
      * Gets all discounts
-     * 
-     * @param string $resource
-     * @param int $parentId
+     *
+     * @param array $params
      * @return array
      */
-    public function getDiscounts($resource, $parentId, array $params = [])
+    public function getDiscounts(array $params = [])
     {
-        return $this->client->get("$resource/$parentId/kb_position_discount", $params);
+        return $this->client->get("{$this->parentResource}/{$this->parentId}/kb_position_discount", $params);
     }
 
     /**
      * Search for discounts
      *
-     * @param string $resource 
-     * @param int $parentId
      * @param array $params
      * @return mixed
      */
-    public function searchDiscounts($resource, $parentId, array $params = [])
+    public function searchDiscounts(array $params = [])
     {
-        return $this->client->post("$resource/$parentId/kb_position_discount", $params);
+        return $this->client->post("{$this->parentResource}/{$this->parentId}/kb_position_discount", $params);
     }
 
     /**
      * Get specific discount
      *
-     * @param string $resource
-     * @param $parentId
      * @param $id
      * @return mixed
      */
-    public function getDiscount($resource, $parentId, $id)
+    public function getDiscount($id)
     {
-        return $this->client->get("$resource/$parentId/kb_position_discount/" . $id, []);
+        return $this->client->get("{$this->parentResource}/{$this->parentId}/kb_position_discount/" . $id, []);
     }
 
     /**
      * Add new discount
-     * 
-     * @param string $resource
-     * @param int $parentId
+     *
      * @param array $params
      * @return mixed
      */
-    public function createDiscount($resource, $parentId, $params = [])
+    public function createDiscount($params = [])
     {
-        return $this->client->post("$resource/$parentId/kb_position_discount", $params);
+        return $this->client->post("{$this->parentResource}/{$this->parentId}/kb_position_discount", $params);
     }
 
 }
